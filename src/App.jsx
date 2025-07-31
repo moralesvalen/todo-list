a4be1baedcefb76a22fbbeadab1b46dcb512ebf8
Jose Mauricio Morales <mauricio.moralesvalen@gmail.com>
2025-07-31 18:42:35 -0400

    Updates based on reviewer feedback

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,26 @@ import { useState } from 'react';
 
 function App() {
   const [newTodo, setNewTodo] = useState('');
+  const [todos, setTodos] = useState([
+    { id: 1, title: 'Review resources' },
+    { id: 2, title: 'Takes Notes' },
+    { id: 3, title: 'Code out app' },
+  ]);
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm />
-      {/*Instancia de TodoForm  <TodoForm /> */}
+      <TodoForm
+        newTodo={newTodo}
+        setNewTodo={setNewTodo}
+        todos={todos}
+        setTodos={setTodos}
+      />
+      {/*Instancia de TodoForm  <TodoForm /> sending state to todoForm*/}
 
       <p>{newTodo}</p>
       {/*Valor del estado */}
 
-      <TodoList />
+      <TodoList todos={todos} />
       {/*Instancia de TodoList  <TodoList /> */}
     </div>
   );
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,12 +2,12 @@ import TodoListItem from './TodoListItem';
 {
   /*extract from TodoList.jsx*/
 }
-function TodoList() {
-  const todos = [
+function TodoList({todos}) {
+ /* const todos = [
     { id: 1, title: 'Review resources' },
     { id: 2, title: 'Takes Notes' },
     { id: 3, title: 'Code out app' },
-  ];
+  ];lo pasaremos desde app.jsx*/
   return (
     <ul>
       {todos.map((todo) => (
