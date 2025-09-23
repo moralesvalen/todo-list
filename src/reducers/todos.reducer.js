3f487cdfdbfba9c0ab9adf317f4f575fa52e5b5b
Jose Mauricio Morales <mauricio.moralesvalen@gmail.com>
2025-09-22 20:45:44 -0400

    chore(reducer): add skeleton (named exports) and wire useReducer

---
+++ src/reducers/todos.reducer.js
@@ -0,0 +1,57 @@
+const initialState = {
+  todoList: [],
+  isLoading: false,
+  isSaving: false,
+  errorMessage: '',
+};
+
+export const actions = {
+  fetchTodos: 'fetchTodos',
+  loadTodos: 'loadTodos',
+  setLoadError: 'setLoadError',
+  startRequest: 'startRequest',
+  addTodo: 'addTodo',
+  endRequest: 'endRequest',
+  updateTodo: 'updateTodo',
+  completeTodo: 'completeTodo',
+  revertTodo: 'revertTodo',
+  clearError: 'clearError',
+};
+
+export function reducer(state = initialState, action) {
+  switch (action.type) {
+    case actions.fetchTodos:
+      return { ...state };
+
+    case actions.loadTodos:
+      return { ...state };
+
+    case actions.setLoadError:
+      return { ...state };
+
+    case actions.startRequest:
+      return { ...state };
+
+    case actions.addTodo:
+      return { ...state };
+
+    case actions.endRequest:
+      return { ...state };
+
+    case actions.updateTodo:
+      return { ...state };
+
+    case actions.completeTodo:
+      return { ...state };
+
+    case actions.revertTodo:
+      return { ...state };
+
+    case actions.clearError:
+      return { ...state };
+
+    default:
+      return state;
+  }
+}
+export default reducer;
